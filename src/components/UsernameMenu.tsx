5b80318fe764ca429ce308673de11de6a47015f2
Kwanusu <kwanusujoseph@gmail.com>
2024-08-10 20:42:03 +0300

    initial commit

---
+++ src/components/UsernameMenu.tsx
@@ -0,0 +1,35 @@
+import { CircleUserRound} from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { useAuth0 } from "@auth0/auth0-react"
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+
+
+const UsernameMenu = () => {
+    const { user, logout } = useAuth0();
+  return (
+    <DropdownMenu>
+        <DropdownMenuTrigger className="flex items-center font-bold hover:text-orange-500 gap-2">
+            <CircleUserRound className="text-orange-500"/>
+            {user?.email}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuItem>
+            <Link to="/user-profile" className="font-bold hover:text-orange-500">
+              User Profile
+            </Link>  
+            </DropdownMenuItem>
+            <Separator/>
+            <DropdownMenuItem>
+            <Button className="font-bold flex flex-1 bg-orange-500"
+            onClick={() => logout()}>
+              Log Out
+            </Button> 
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UsernameMenu
